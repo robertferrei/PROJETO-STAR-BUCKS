fa0bc13a60970cf74249865baa76f464d6faa448
robertferrei <ferreirarobert727@gmail.com>
2023-03-10 19:26:08 -0300

    starbucks

---
+++ index.js
@@ -0,0 +1,11 @@
+let imagem = document.querySelector(".starbuckss")
+let circuloo = document.querySelector(".circuloo")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+
+function trocaCor(Cor) {
+    circuloo.style.background = Cor
+
+}
